ce4e90b252a520fd55529709e70487ba6b9dbf7c
Darius Thomas <dariusd.thomas@gmail.com>
2019-04-03 01:42:10 -0400

    chipper (front end complete)

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            submitedText: ['First Chirp', 'Second Chirp', 'Third Chirp'],
+            text: '',
+            curTime: new Date().toLocaleString()
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+    handleClick() {
+        let arr = [...this.state.submitedText];
+        arr.unshift(this.state.text);
+        this.setState({
+            submitedText: arr,
+            text: ''
+        })
+    }
+    render() {
+        let messageList = this.state.submitedText.map((i, index) => <div class=" border border-dark w-100 "><div class="border" >{i}</div><p>{this.state.curTime}</p></div>)
+        return (
+            <>
+                    <div>
+                        <input type="text" placeholder="Enter Chirp" onChange={this.handleChange} />
+                        <input type="button" value="submit" onClick={this.handleClick} className="btn btn-primary" />
+                    </div>
+                    <div className="d-flex p-4 flex-wrap flex-column align-items-center">
+                         {messageList} 
+                    </div>
+            </>
+        )
+    }
+}
+
+export default App
